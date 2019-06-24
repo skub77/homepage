2f3e3840e075745562ac4c6fa0d03b5c1558f3fe
Marcin Gemra <gemza25@gmai.com>
2019-06-24 12:57:34 +0200

    modified class names

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ let number = 5;
 
 console.log(`Moje imie to: ${name} 
 a liczba: ${number}`);
+
+const header = document.querySelector('.header');
+console.log(header);
+
+const paragraphs = document.querySelectorAll('p');
+console.log(paragraphs[2]);
+console.log(paragraphs[2].innerHTML);
+
+paragraphs[2].innerHTML = "Pozdro JS"
